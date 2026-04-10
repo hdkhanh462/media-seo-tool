013602532b75da3b5685fda71be60f097338e452
Hoàng Dương Khánh <101619460+hdkhanh462@users.noreply.github.com>
2026-04-10 15:13:26 +0700

    fix: Update app width

--- src/mainview/App.tsx
+++ src/mainview/App.tsx
@@ -6,7 +6,7 @@ import { SidebarInset } from "@/components/ui/sidebar";
 function App() {
   return (
     <SidebarInset>
-      <div className="flex max-h-[calc(100vh-var(--header-height))] max-w-[calc(100vw-var(--container-md)-var(--sidebar-width-icon))] flex-1 flex-col gap-4 overflow-y-auto p-4">
+      <div className="flex max-h-[calc(100vh-var(--header-height))] lg:max-w-[calc(100vw-var(--container-md)-var(--sidebar-width-icon))] flex-1 flex-col gap-4 overflow-y-auto p-4">
         <AppContent value="editor">
           <EditorContainer />
         </AppContent>
